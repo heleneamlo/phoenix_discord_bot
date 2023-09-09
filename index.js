4149b6e5a9cd1902c162422a63cf48c7dbb3447e
heleneamlo <amlohenning@gmail.com>
2023-09-09 14:12:11 +0200

    jvneafkjnfakjnf

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ const {
 } = require('discord.js');
 const dotenv = require('dotenv').config();
 const phoenix = require('@phoenixlan/phoenix.js');
-const amqp = require('amqplib');
+const amqp = require('amqplib/callback_api');
 
 const DISCORD_TOKEN = process.env.BOT_TOKEN;
-const RABBITMQ_URL = 'YOUR_RABBITMQ_URL';
-const QUEUE_NAME = 'your-queue-name';
+amqp.connect('amqp://phoenix:testing@127.0.0.1:5672', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = 'position_changes';
 
+
+    channel.assertQueue(queue, {
+      durable: true
+    });
+
+    channel.consume(queue, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+        let messageuuid = msg.content.toString().split(' ');
+        console.log(messageuuid);
+    }, {
+        noAck: true
+    });
+
+  });
+});
 const phoenixClient = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -20,51 +42,11 @@ const phoenixClient = new Client({
     ],
 });
 
-const roleData = [{
-    "discord_id": "371352013432422411",
-    "member": true,
-    "chief": false
-}, {
-    "discord_id": "853001789849862175",
-    "member": true,
-    "chief": true
-}, {
-    "discord_id": "377816145589174285",
-    "member": false,
-    "chief": false
-}, {
-    "discord_id": "439121172467154946",
-    "member": false,
-    "chief": true
-}]
-
-
-
 async function updateRoles() {
     const targetGuild = phoenixClient.guilds.cache.first();
-    roleData.forEach(async (userData) => {
-        parseInt(userData.discord_id);
-        try {
-            const member = await targetGuild.members.fetch(userData.discord_id);
-            if (member) {
-                // Check and add roles if needed
-                if (userData.member && !member.roles.cache.some(role => role.name == 'Crew')) {
-                    const memberRole = targetGuild.roles.cache.find(role => role.name == 'Crew');
-                    if (memberRole) {
-                        member.roles.add(memberRole);
-                    }
-                }
-                if (userData.chief && !member.roles.cache.some(role => role.name == 'Gruppeleder')) {
-                    const chiefRole = targetGuild.roles.cache.find(role => role.name == 'Gruppeleder');
-                    if (chiefRole) {
-                        member.roles.add(chiefRole);
-                    }
-                }
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    });
+    let crews = await Promise.all((await phoenix.Crew.getCrews()))
+    crews.forEach(async (Crew) => {
+        let allCrews = await phoenix.Crew.getCrew(Crew.uuid) })
 }
 
 
@@ -144,5 +126,6 @@ phoenixClient.on('messageCreate', async (message) => {
         }
     }
 });
-
+phoenix.init("https://api.test.phoenixlan.no");
+phoenix.User.Oauth.setAuthState("eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJ0aWNrZXRfd2hvbGVzYWxlIiwibWVtYmVyIiwidXNlcjplNWQ1ZGMxOC1kZjk5LTRkMWYtYjQ0OC1iYWEyYTg5ZWY3MGUiLCJhZG1pbiIsImNoaWVmOjIzODZkMGE1LTdjYTMtNGVlOS1iNTExLTI2MTNhMjE2ZWZhOSIsImNoaWVmIiwidGlja2V0X2J5cGFzc190aWNrZXRzYWxlX3N0YXJ0X3Jlc3RyaWN0aW9uIl0sImZsYWciOiJQSE9FTklYe0pXVFNfQVJFX0FXRVNPTUV9Iiwic3ViIjoiZTVkNWRjMTgtZGY5OS00ZDFmLWI0NDgtYmFhMmE4OWVmNzBlIiwiaWF0IjoxNjkyNjM1OTU5LCJleHAiOjE2OTI2Mzk1NTl9.Bz_Bjg9tFaaJdFHu6fLWMZh7r9eM0EstxIoaIQAAyKw2fdOpvRdh5fQ9rRu883KYAi2yNYn93WSFb8UmW4IfmQ", "FqzfMCdgrsKlMZKLgQhpSUkOWjxCQVLwNATzAlMR");
 phoenixClient.login(DISCORD_TOKEN);
